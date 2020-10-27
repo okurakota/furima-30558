f4c7aa65bf275d8b19f9510ae6ff99f91fc8c306
okurakota <okura.kota01@gmail.com>
2020-10-27 18:10:58 +0900

    ActiveHashを導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
