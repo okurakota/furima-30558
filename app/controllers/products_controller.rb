fbfebc9c3f4e9bb758e7daa68747448c13a876ee
okurakota <okura.kota01@gmail.com>
2020-10-31 13:03:36 +0900

    編集前情報を表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,12 +23,15 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
   end
 
   def destroy
+    product = Product.find(params[:id])
   end
 
   private
