3ad1d4617eca43f50c6db28192b87a37ba2fa97a
okurakota <okura.kota01@gmail.com>
2020-11-06 15:43:26 +0900

    購入機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,7 +7,9 @@ class PurchasesController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @address_purchase = AddressPurchase.new(purchase_params)
+    #binding.pry
     if @address_purchase.valid?
+      pay_item
       @address_purchase.save
       redirect_to root_path
     else
@@ -18,6 +20,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.permit(:postal_code, :prefectures_id, :municipalities, :addresses, :building_name, :phone_number, :purchase_id, :user_id, :product_id).merge(user_id: current_user.id)
+    params.permit(:postal_code, :prefectures_id, :municipalities, :addresses, :building_name, :phone_number, :purchase_id, :user_id, :product_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @product.price,  # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
   end
 end
